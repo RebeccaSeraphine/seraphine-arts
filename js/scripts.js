022535ce3c3696ca00a4c94b0eb18b09faec22f8
RebeccaSeraphine <rebecca.s.mueller@web.de>
2019-08-06 17:46:22 +0200

    product images added to shop

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
+    $('.carousel').carousel('pause');
+});
+
+console.log("hello");
+
+/* Carousel auto play
+$(document).ready(function() {
     $('#mycarousel').carousel({
         interval: 10
     })
 
-});
-
-console.log("hello");
+}); */
