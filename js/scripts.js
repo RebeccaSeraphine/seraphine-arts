d94b04d5238ddd3435e72b63719c0f0e683b0904
RebeccaSeraphine <rebecca.s.mueller@web.de>
2020-05-01 15:06:58 +0200

    adds subnavigation for galleries

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,56 @@ $(document).ready(function() {
     })
 
 }); */
+
+// Sub-Navigation for Gallery view
+
+function animalsSelected() {
+  var zentangleAnimals = document.getElementById("zentangle-animals-gallery");
+  var spaceTravel = document.getElementById("space-travel-gallery");
+  var watercolour = document.getElementById("watercolour-gallery");
+  var justForFun = document.getElementById("fun-gallery");
+  var animalGallery = document.getElementById("zentangle-animals");
+  var watercolorGallery = document.getElementById(
+    "watercolour-paintings-gallery"
+  );
+
+  zentangleAnimals.className = "nav-link gallerynavigation-active";
+  spaceTravel.className = "nav-link gallerynavigation-not-active";
+  watercolour.className = "nav-link gallerynavigation-not-active";
+  justForFun.className = "nav-link gallerynavigation-not-active";
+  watercolourGallery.classname = "container-fluid gallery-hide";
+  animalGallery.className = "container-fluid gallery-show";
+}
+
+function travelSelected() {
+  var zentangleAnimals = document.getElementById("zentangle-animals-gallery");
+  var spaceTravel = document.getElementById("space-travel-gallery");
+  var watercolour = document.getElementById("watercolour-gallery");
+  var justForFun = document.getElementById("fun-gallery");
+  zentangleAnimals.className = "nav-link gallerynavigation-not-active";
+  spaceTravel.className = "nav-link gallerynavigation-active";
+  watercolour.className = "nav-link gallerynavigation-not-active";
+  justForFun.className = "nav-link gallerynavigation-not-active";
+}
+
+function watercolourSelected() {
+  var zentangleAnimals = document.getElementById("zentangle-animals-gallery");
+  var spaceTravel = document.getElementById("space-travel-gallery");
+  var watercolour = document.getElementById("watercolour-gallery");
+  var justForFun = document.getElementById("fun-gallery");
+  zentangleAnimals.className = "nav-link gallerynavigation-not-active";
+  spaceTravel.className = "nav-link gallerynavigation-not-active";
+  watercolour.className = "nav-link gallerynavigation-active";
+  justForFun.className = "nav-link gallerynavigation-not-active";
+}
+
+function funSelected() {
+  var zentangleAnimals = document.getElementById("zentangle-animals-gallery");
+  var spaceTravel = document.getElementById("space-travel-gallery");
+  var watercolour = document.getElementById("watercolour-gallery");
+  var justForFun = document.getElementById("fun-gallery");
+  zentangleAnimals.className = "nav-link gallerynavigation-not-active";
+  spaceTravel.className = "nav-link gallerynavigation-not-active";
+  watercolour.className = "nav-link gallerynavigation-not-active";
+  justForFun.className = "nav-link gallerynavigation-active";
+}
