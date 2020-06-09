2f24206214291ad57bb40681d6a35af3c0abc7b7
RebeccaSeraphine <rebecca.s.mueller@web.de>
2020-06-09 13:45:02 +0200

    add watercolour gallery modals

--- js/gallery-watercolour-modal-views.js
+++ js/gallery-watercolour-modal-views.js
@@ -1,9 +1,8 @@
-var watercolourGallery = document.getElementById("script-watercolour-gallery")
-  .innerHTML;
+var modalViewsWatercolour = document.getElementById("script-modal-views-watercolour").innerHTML;
 
-var template = Handlebars.compile(watercolourGallery);
+var template = Handlebars.compile(modalViewsWatercolour);
 
-var watercolourData = template({
+var modalDataWatercolour = template({
   images: [
     // Spirits of the sea
     {
@@ -11,18 +10,21 @@ var watercolourData = template({
       date: "2019",
       src: "public/images/gallery/lady-manta.jpg",
       alt: "manta woman watercolour",
+      id: "w-manta-woman",
     },
     {
       name: "Turtle Priestess",
       date: "2019",
       src: "public/images/gallery/turtle-priestess.jpg",
       alt: "turtle woman watercolour",
+      id: "w-turtle-woman",
     },
     {
       name: "Seahorse Guardian",
       date: "2019",
       src: "public/images/gallery/seahorse-guardian.jpg",
       alt: "seahorse woman watercolour",
+      id: "w-seahorse-woman",
     },
     // Single
     {
@@ -30,6 +32,7 @@ var watercolourData = template({
       date: "2019",
       src: "public/images/gallery/vietnamese-girl.jpg",
       alt: "dancing vietnamese girl painting",
+      id: "w-vietnamesedancer",
     },
     // Patterns of Nature
     {
@@ -37,24 +40,28 @@ var watercolourData = template({
       date: "2018",
       src: "public/images/gallery/lion.jpg",
       alt: "lion watercolour",
+      id: "w-lion",
     },
     {
       name: "Patterns of Nature - Wolf",
       date: "2018",
       src: "public/images/gallery/wolf.jpg",
       alt: "wolf watercolour",
+      id: "w-wolf",
     },
     {
       name: "Patterns of Nature - Monkey",
       date: "2018",
       src: "public/images/gallery/monkey.jpg",
       alt: "monkey watercolour",
+      id: "w-monkey",
     },
     {
       name: "Patterns of Nature - Monstera Leaf",
       date: "2018",
       src: "public/images/gallery/monstera-leaf.jpg",
       alt: "monstera watercolour painting",
+      id: "w-monstera",
     },
     // Single
     {
@@ -62,8 +69,9 @@ var watercolourData = template({
       date: "2019",
       src: "public/images/gallery/bamboo-forest.jpg",
       alt: "bamboo watercolor painting",
+      id: "w-bambooforest",
     },
   ],
 });
 
-document.getElementById("watercolourGallery").innerHTML += watercolourData;
+document.getElementById("div-modalViews-watercolour").innerHTML += modalDataWatercolour;
